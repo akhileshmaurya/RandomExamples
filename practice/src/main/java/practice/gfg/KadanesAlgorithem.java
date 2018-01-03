a4e4f3e791d6ea80b0a7512b24c2f6b574cb659c
Akhilesh Kumar Maurya <maurya_akhilesh@ymail.com>
2018-01-03 21:47:17 +0530

    solving some easy problems

---
+++ practice/src/main/java/practice/gfg/KadanesAlgorithem.java
@@ -0,0 +1,36 @@
+package practice.gfg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
+public class KadanesAlgorithem {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int[] inputNumbers = convertStringToNumber(br.readLine().trim().split(" "));
+			int globalMax = inputNumbers[0];
+			int localMax = globalMax;
+			for (int j = 1; j < n; j++) {
+				localMax = Math.max(inputNumbers[j], localMax + inputNumbers[j]);
+				globalMax = Math.max(globalMax, localMax);
+			}
+			System.out.println(globalMax);
+		}
+
+	}
+
+	private static int[] convertStringToNumber(String[] split) {
+		int[] inputNumbers = new int[split.length];
+		int i = 0;
+		for (String string : split) {
+			inputNumbers[i++] = Integer.parseInt(string.trim());
+		}
+		return inputNumbers;
+	}
+
+}
---
+++ practice/src/main/java/practice/gfg/MissingNumber.java
@@ -0,0 +1,35 @@
+package practice.gfg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//https://practice.geeksforgeeks.org/problems/missing-number-in-array/0
+public class MissingNumber {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int[] inputNumbers = convertStringToNumber(br.readLine().trim().split(" "));
+			int globalSum = 0;
+			for (int j = 0; j < n - 1; j++) {
+				globalSum = globalSum + inputNumbers[j];
+			}
+			int actualSum = n * (n + 1) / 2;
+			System.out.println(actualSum - globalSum);
+		}
+
+	}
+
+	private static int[] convertStringToNumber(String[] split) {
+		int[] inputNumbers = new int[split.length];
+		int i = 0;
+		for (String string : split) {
+			inputNumbers[i++] = Integer.parseInt(string.trim());
+		}
+		return inputNumbers;
+	}
+
+}
