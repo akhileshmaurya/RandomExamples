c876f4f0030bb8d824ed3abb73415a6f5410de0b
Akhilesh Kumar Maurya <maurya_akhilesh@ymail.com>
2018-01-07 20:56:20 +0530

    Some Practise 7 jan

---
+++ practice/src/main/java/practice/furkan/OnionArray.java
@@ -0,0 +1,36 @@
+package practice.furkan;
+
+public class OnionArray {
+
+	public static void main(String[] args) {
+		System.out.println(isOnionArray(new int[] { 1, 2, 19, 4, 5 }));
+		System.out.println(isOnionArray(new int[] { 1, 2, 3, 4, 15 }));
+		System.out.println(isOnionArray(new int[] { 1, 3, 9, 8 }));
+		System.out.println(isOnionArray(new int[] { 2 }));
+		System.out.println(isOnionArray(new int[] {}));
+		System.out.println(isOnionArray(new int[] { -2, 5, 0, 5, 12 }));
+	}
+
+	public static int isOnionArray(int a[]) {
+		if (a.length <= 1)
+			return 1;
+
+		int isOnionArray = 0;
+		boolean mitchmatch = false;
+
+		for (int i = 0; i < a.length / 2; i++) {
+			if ((a[i] + a[a.length - 1 - i]) > 10) {
+				mitchmatch = true;
+				break;
+			} else {
+				isOnionArray = 1;
+			}
+
+		}
+		if (mitchmatch)
+			isOnionArray = 0;
+
+		return isOnionArray;
+	}
+
+}
---
+++ practice/src/main/java/practice/furkan/TwinPair.java
@@ -0,0 +1,41 @@
+package practice.furkan;
+
+public class TwinPair {
+
+	public static void main(String[] args) {
+		System.out.println(isTwinPaired(new int[] { 2, 4, 32 }));
+		System.out.println(isTwinPaired(new int[] { 1, 1, 1 }));
+		System.out.println(isTwinPaired(new int[] { 1, 19, 23 }));
+		System.out.println(isTwinPaired(new int[] { 1, 2, 2, 1 }));
+		System.out.println(isTwinPaired(new int[] { 8 }));
+		System.out.println(isTwinPaired(new int[] { 17 }));
+		System.out.println(isTwinPaired(new int[] {}));
+	}
+
+	public static int isTwinPaired(int a[]) {
+		if (a.length <= 1)
+			return 1;
+		boolean isOddInc = true;
+		boolean isEvenInc = true;
+		Integer lastOdd = null;
+		Integer lastEven = null;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 2 == 0) {
+				if (lastEven != null && lastEven > a[i]) {
+					isEvenInc = false;
+				}
+				lastEven = a[i];
+			} else {
+				if (lastOdd != null && lastOdd > a[i]) {
+					isOddInc = false;
+				}
+				lastOdd = a[i];
+			}
+			if (!isOddInc || !isEvenInc) {
+				break;
+			}
+		}
+		return (isOddInc && isEvenInc) ? 1 : 0;
+	}
+
+}
